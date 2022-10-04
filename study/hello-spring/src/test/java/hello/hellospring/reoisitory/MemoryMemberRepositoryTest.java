319b2ce36e1b9f83a22911edceecbf0a9d0a35d4
Dingadung <1229jimin@naver.com>
2022-10-04 19:24:49 +0900

    회원 도메인과 리포지토리 만들기:

---
+++ study/hello-spring/src/test/java/hello/hellospring/reoisitory/MemoryMemberRepositoryTest.java
@@ -0,0 +1,38 @@
+package hello.hellospring.reoisitory;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+    }
+
+}
