dae634605f1aa4bf91f8ce3ce65629697f35e656
Dingadung <1229jimin@naver.com>
2023-01-06 18:41:13 +0900

    .

---
+++ study/core/src/main/java/HelloLombok.java
@@ -0,0 +1,14 @@
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloLombok {
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok();
+        System.out.println(helloLombok.getName());
+    }
+}
