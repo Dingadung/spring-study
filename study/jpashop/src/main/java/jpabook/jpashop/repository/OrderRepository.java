4888d2c0a2e1e3e665ea12e4ca6719e193707ce0
Dingadung <1229jimin@naver.com>
2023-04-06 20:43:41 +0900

    Add: 주문도메인개발

---
+++ study/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,43 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderSeach;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+       return em.find(Order.class, id);
+    }
+
+    public List<Order> findAll(OrderSeach orderSeach) {
+        String jpql = "select o from Order o join o.member m";
+        return em.createQuery(jpql)
+//                " where o.status =: status" +
+//                " and m.name like :name", Order.class)
+//                .setParameter("status", orderSeach.getOrderStatus())
+//                .setParameter("name", orderSeach.getMemberName())
+                .setMaxResults(1000) // 최대 1000건
+                .getResultList();
+    }
+
+    /**
+     *
+     * JPA Criteria
+     */
+//    public List<Order> findAllByCriteria(OrderSeach orderSeach) {
+//
+//    }
+}
